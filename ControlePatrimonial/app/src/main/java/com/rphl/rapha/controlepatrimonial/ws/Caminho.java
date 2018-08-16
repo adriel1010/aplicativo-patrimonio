b64a9162411465170a69f85451535531610784e6
adriel <adrielcarlos1010@gmail.com>
2018-08-16 10:38:02 -0300

    alterando url

--- ControlePatrimonial/app/src/main/java/com/rphl/rapha/controlepatrimonial/ws/Caminho.java
+++ ControlePatrimonial/app/src/main/java/com/rphl/rapha/controlepatrimonial/ws/Caminho.java
@@ -9,7 +9,8 @@ public class Caminho {
 
     public static String Caminho(){
 
-        //return "http://200.17.98.122:8080/cronos/rest/service";
-        return "http://172.21.150.124:8080/cronos/rest/service";
+        return "http://200.17.98.122:8080/cronos/rest/service";
+       // return "http://172.21.150.124:8080/cronos/rest/service";
+
     }
 }
